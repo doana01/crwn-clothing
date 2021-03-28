f70a780edf558c56fa98b94a46c2055db9e5a7ed
doana01 <artlovevintage@gmail.com>
2021-03-28 12:16:34 +0200

    adding header component and lonks to home, shop, page and contact

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+
 
 
 function App() {
   return (
     <div>
+      <Header />
         <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -18,5 +22,4 @@ function App() {
 }
 
 export default App;
-
 
